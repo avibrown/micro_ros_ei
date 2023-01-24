49274e28e531a1ee65a5da14507d36aba4c33bac
Avi Brown <63222803+avielbr@users.noreply.github.com>
2023-01-24 15:28:41 +0200

    Update ei_micro_ros_portenta.ino

--- examples/arduino/ei_micro_ros_portenta/ei_micro_ros_portenta.ino
+++ examples/arduino/ei_micro_ros_portenta/ei_micro_ros_portenta.ino
@@ -1,18 +1,23 @@
-#include <micro_ros_arduino.h>
+#define EIDSP_QUANTIZE_FILTERBANK   0
 
+/* Includes ---------------------------------------------------------------- */
+#include <PDM.h>
+#include <micro_ros_arduino.h>
 #include <stdio.h>
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-//#include <std_msgs/msg/int32.h>
 // Custom messages
 #include <ei_interfaces/msg/ei_result.h>
 #include <ei_interfaces/msg/ei_classification.h>
 
+// Change this to match your own custom model library
+#include <micro_ros_wakeword_inferencing.h>
+
 rcl_publisher_t publisher;
-//std_msgs__msg__Int32 msg;
+
 // Initialize result msg
 ei_interfaces__msg__EIResult msg;
 
@@ -27,290 +32,21 @@ rcl_timer_t timer;
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
-///
-
-// Replace this with <name_of_ei_library_inferencing.h>
-#include <dog_cat_inferencing.h>
-
-#include "camera.h"
-#include "himax.h"
-#include "edge-impulse-sdk/dsp/image/image.hpp"
-
-/* Constant defines -------------------------------------------------------- */
-#define EI_CAMERA_RAW_FRAME_BUFFER_COLS 320
-#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS 240
-
-#define EI_TIMER                        100
-
-#define EI_CAMERA_FRAME_BUFFER_SDRAM
-
-#ifdef EI_CAMERA_FRAME_BUFFER_SDRAM
-#include "SDRAM.h"
-#endif
-
-#define ALIGN_PTR(p,a)   ((p & (a-1)) ?(((uintptr_t)p + a) & ~(uintptr_t)(a-1)) : p)
-
-
-/* Edge Impulse ------------------------------------------------------------- */
-
+/** Audio buffers, pointers and selectors */
 typedef struct {
-    size_t width;
-    size_t height;
-} ei_device_resize_resolutions_t;
+    signed short *buffers[2];
+    unsigned char buf_select;
+    unsigned char buf_ready;
+    unsigned int buf_count;
+    unsigned int n_samples;
+} inference_t;
 
-/* Private variables ------------------------------------------------------- */
+static inference_t inference;
+static volatile bool record_ready = false;
+// static signed short *sampleBuffer;
+static signed short sampleBuffer[2048];
 static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
-static bool is_initialised = false;
-static bool is_ll_initialised = false;
-HM01B0 himax;
-static Camera cam(himax);
-FrameBuffer fb;
-
-/*
-** @brief points to the output of the capture
-*/
-static uint8_t *ei_camera_capture_out = NULL;
-
-/*
-** @brief used to store the raw frame
-*/
-#if defined(EI_CAMERA_FRAME_BUFFER_SDRAM) || defined(EI_CAMERA_FRAME_BUFFER_HEAP)
-static uint8_t *ei_camera_frame_mem;
-static uint8_t *ei_camera_frame_buffer; // 32-byte aligned
-#else
-static uint8_t ei_camera_frame_buffer[EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS] __attribute__((aligned(32)));
-#endif
-
-/* Function definitions ------------------------------------------------------- */
-bool ei_camera_init(void);
-void ei_camera_deinit(void);
-bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) ;
-int calculate_resize_dimensions(uint32_t out_width, uint32_t out_height, uint32_t *resize_col_sz, uint32_t *resize_row_sz, bool *do_resize);
-
-
-///
-
-
-/**
- * @brief   Setup image sensor & start streaming
- *
- * @retval  false if initialisation failed
- */
-bool ei_camera_init(void) {
-    if (is_initialised) return true;
-
-    if (is_ll_initialised == false) {
-        if (!cam.begin(CAMERA_R320x240, CAMERA_GRAYSCALE, 30)) {
-            return false;
-        }
-
-    #ifdef EI_CAMERA_FRAME_BUFFER_SDRAM
-        ei_camera_frame_mem = (uint8_t *) SDRAM.malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS + 32 /*alignment*/);
-        if(ei_camera_frame_mem == NULL) {
-            return false;
-        }
-        ei_camera_frame_buffer = (uint8_t *)ALIGN_PTR((uintptr_t)ei_camera_frame_mem, 32);
-    #endif
-
-        is_ll_initialised = true;
-    }
-
-    // initialize frame buffer
-#if defined(EI_CAMERA_FRAME_BUFFER_HEAP)
-    ei_camera_frame_mem = (uint8_t *) ei_malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS + 32 /*alignment*/);
-    if(ei_camera_frame_mem == NULL) {
-        return false;
-    }
-    ei_camera_frame_buffer = (uint8_t *)ALIGN_PTR((uintptr_t)ei_camera_frame_mem, 32);
-#endif
-
-    fb.setBuffer(ei_camera_frame_buffer);
-    is_initialised = true;
-
-    return true;
-}
-
-/**
- * @brief      Stop streaming of sensor data
- */
-void ei_camera_deinit(void) {
-
-#if defined(EI_CAMERA_FRAME_BUFFER_HEAP)
-    ei_free(ei_camera_frame_mem);
-    ei_camera_frame_mem = NULL;
-    ei_camera_frame_buffer = NULL;
-#endif
-
-    is_initialised = false;
-}
-
-/**
- * @brief      Capture, rescale and crop image
- *
- * @param[in]  img_width     width of output image
- * @param[in]  img_height    height of output image
- * @param[in]  out_buf       pointer to store output image, NULL may be used
- *                           if ei_camera_frame_buffer is to be used for capture and resize/cropping.
- *
- * @retval     false if not initialised, image captured, rescaled or cropped failed
- *
- */
-bool ei_camera_capture(uint32_t img_width, uint32_t img_height, uint8_t *out_buf) {
-    bool do_resize = false;
-    bool do_crop = false;
-
-    if (!is_initialised) {
-        return false;
-    }
-
-    int snapshot_response = cam.grabFrame(fb, 3000);
-    if (snapshot_response != 0) {
-        return false;
-    }
-
-    uint32_t resize_col_sz;
-    uint32_t resize_row_sz;
-    // choose resize dimensions
-    int res = calculate_resize_dimensions(img_width, img_height, &resize_col_sz, &resize_row_sz, &do_resize);
-    if (res) {
-        return false;
-    }
-
-    if ((img_width != resize_col_sz)
-        || (img_height != resize_row_sz)) {
-        do_crop = true;
-    }
-
-    // The following variables should always be assigned
-    // if this routine is to return true
-    // cutout values
-    ei_camera_capture_out = ei_camera_frame_buffer;
-
-    if (do_resize) {
-
-        // if only resizing then and out_buf provided then use itinstead.
-        if (out_buf && !do_crop) ei_camera_capture_out = out_buf;
-
-        ei::image::processing::resize_image(
-            ei_camera_frame_buffer,
-            EI_CAMERA_RAW_FRAME_BUFFER_COLS,
-            EI_CAMERA_RAW_FRAME_BUFFER_ROWS,
-            ei_camera_capture_out,
-            resize_col_sz,
-            resize_row_sz,
-            1); // bytes per pixel
-    }
-
-    if (do_crop) {
-        uint32_t crop_col_sz;
-        uint32_t crop_row_sz;
-        uint32_t crop_col_start;
-        uint32_t crop_row_start;
-        crop_row_start = (resize_row_sz - img_height) / 2;
-        crop_col_start = (resize_col_sz - img_width) / 2;
-        crop_col_sz = img_width;
-        crop_row_sz = img_height;
-
-        // if (also) cropping and out_buf provided then use it instead.
-        if (out_buf) ei_camera_capture_out = out_buf;
-
-        ei::image::processing::cropImage(
-            ei_camera_frame_buffer,
-            resize_col_sz,
-            resize_row_sz,
-            crop_col_start,
-            crop_row_start,
-            ei_camera_capture_out,
-            crop_col_sz,
-            crop_row_sz,
-            8); // bits per pixel
-    }
-
-    return true;
-}
-
-/**
- * @brief      Convert monochrome data to rgb values
- *
- * @param[in]  mono_data  The mono data
- * @param      r          red pixel value
- * @param      g          green pixel value
- * @param      b          blue pixel value
- */
-static inline void mono_to_rgb(uint8_t mono_data, uint8_t *r, uint8_t *g, uint8_t *b) {
-    uint8_t v = mono_data;
-    *r = *g = *b = v;
-}
-
-
-int ei_camera_cutout_get_data(size_t offset, size_t length, float *out_ptr) {
-    size_t bytes_left = length;
-    size_t out_ptr_ix = 0;
-
-    // read byte for byte
-    while (bytes_left != 0) {
-
-        // grab the value and convert to r/g/b
-        uint8_t pixel = ei_camera_capture_out[offset];
-
-        uint8_t r, g, b;
-        mono_to_rgb(pixel, &r, &g, &b);
-
-        // then convert to out_ptr format
-        float pixel_f = (r << 16) + (g << 8) + b;
-        out_ptr[out_ptr_ix] = pixel_f;
-
-        // and go to the next pixel
-        out_ptr_ix++;
-        offset++;
-        bytes_left--;
-    }
-
-    // and done!
-    return 0;
-}
-
-/**
- * @brief      Determine whether to resize and to which dimension
- *
- * @param[in]  out_width     width of output image
- * @param[in]  out_height    height of output image
- * @param[out] resize_col_sz       pointer to frame buffer's column/width value
- * @param[out] resize_row_sz       pointer to frame buffer's rows/height value
- * @param[out] do_resize     returns whether to resize (or not)
- *
- */
-int calculate_resize_dimensions(uint32_t out_width, uint32_t out_height, uint32_t *resize_col_sz, uint32_t *resize_row_sz, bool *do_resize)
-{
-    size_t list_size = 6;
-    const ei_device_resize_resolutions_t list[list_size] = {
-        {128, 96},
-        {160, 120},
-        {200, 150},
-        {256, 192},
-        {320, 240},
-    };
-
-    // (default) conditions
-    *resize_col_sz = EI_CAMERA_RAW_FRAME_BUFFER_COLS;
-    *resize_row_sz = EI_CAMERA_RAW_FRAME_BUFFER_ROWS;
-    *do_resize = false;
-
-    for (size_t ix = 0; ix < list_size; ix++) {
-        if ((out_width <= list[ix].width) && (out_height <= list[ix].height)) {
-            *resize_col_sz = list[ix].width;
-            *resize_row_sz = list[ix].height;
-            *do_resize = true;
-            break;
-        }
-    }
-
-    return 0;
-}
-//
-//#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_CAMERA
-//#error "Invalid model for current sensor"
-//#endif
+static int print_results = -(EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW);
 
 void error_loop(){
   while(1){
@@ -324,32 +60,23 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
   RCLC_UNUSED(last_call_time);
   if (timer != NULL) {
     
-    ei::signal_t signal;
-    signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
-    signal.get_data = &ei_camera_cutout_get_data;
-
-    if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, NULL) == false) {
+    bool m = microphone_inference_record();
+    if (!m) {
+        // ei_printf("ERR: Failed to record audio...\n");
         return;
     }
 
-    // Run the classifier
-    ei_impulse_result_t result = { 0 };
-    
-    EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
-    if (err != EI_IMPULSE_OK) {
-        digitalWrite(LEDR, LOW);
-        delay(50);
-        digitalWrite(LEDR, HIGH);
-        return;
-    }
+    signal_t signal;
+    signal.total_length = EI_CLASSIFIER_SLICE_SIZE;
+    signal.get_data = &microphone_audio_signal_get_data;
+    ei_impulse_result_t result = {0};
+
+    EI_IMPULSE_ERROR r = run_classifier_continuous(&signal, &result, debug_nn);
 
     for (int32_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
       strcpy(msg.result.data[ix].label.data, result.classification[ix].label);
       msg.result.data[ix].value = result.classification[ix].value;
     }
-
-//    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-//    msg.data++;
 
   digitalWrite(LEDB, LOW);
   RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
@@ -358,31 +85,38 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
   }
 }
 
-void setup() {
+/**
+ * @brief      Arduino setup function
+ */
+void setup()
+{
+    run_classifier_init();
+    if (microphone_inference_start(EI_CLASSIFIER_SLICE_SIZE) == false) {
+        // ei_printf("ERR: Could not allocate audio buffer (size %d), this could be due to the window length of your model\r\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT);
+        return;
+    }
 
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    delay(100);
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  delay(100);
   set_microros_transports();
   
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);  
-  
-//  delay(2000);
-
+ 
   allocator = rcl_get_default_allocator();
 
   //create init_options
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
   // create node
-  RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_wakeword_node", "", &support));
 
   // Create publisher
   RCCHECK(rclc_publisher_init_default(
     &publisher,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(ei_interfaces, msg, EIResult),
-    "/ei_ros_publisher"));
+    "/wakeword_publisher"));
 
   // create timer,
   const unsigned int timer_timeout = 50;
@@ -409,26 +143,144 @@ void setup() {
     msg.result.size++;
   }
 
-  #ifdef EI_CAMERA_FRAME_BUFFER_SDRAM
-      // Initialise the SDRAM
-      SDRAM.begin(SDRAM_START_ADDRESS);
-  #endif
-  
-      ei_camera_init();
-  
-      for (size_t ix = 0; ix < ei_dsp_blocks_size; ix++) {
-          ei_model_dsp_t block = ei_dsp_blocks[ix];
-          if (block.extract_fn == &extract_image_features) {
-              ei_dsp_config_image_t config = *((ei_dsp_config_image_t*)block.config);
-              int16_t channel_count = strcmp(config.channels, "Grayscale") == 0 ? 1 : 3;
-              if (channel_count == 3) {
-                  break;
-              }
-          }
-      }
 }
 
-void loop() {
-  //delay(100);
+/**
+ * @brief      Arduino main function. Runs the inferencing loop.
+ */
+void loop()
+{
   RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
+
+/**
+ * @brief      PDM buffer full callback
+ *             Copy audio data to app buffers
+ */
+static void pdm_data_ready_inference_callback(void)
+{
+    int bytesAvailable = PDM.available();
+
+    // read into the sample buffer
+    int bytesRead = PDM.read((char *)&sampleBuffer[0], bytesAvailable);
+
+    if ((inference.buf_ready == 0) && (record_ready == true)) {
+        for(int i = 0; i < bytesRead>>1; i++) {
+            inference.buffers[inference.buf_select][inference.buf_count++] = sampleBuffer[i];
+
+            if (inference.buf_count >= inference.n_samples) {
+                inference.buf_select ^= 1;
+                inference.buf_count = 0;
+                inference.buf_ready = 1;
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * @brief      Init inferencing struct and setup/start PDM
+ *
+ * @param[in]  n_samples  The n samples
+ *
+ * @return     { description_of_the_return_value }
+ */
+static bool microphone_inference_start(uint32_t n_samples)
+{
+    inference.buffers[0] = (signed short *)malloc(n_samples * sizeof(signed short));
+
+    if (inference.buffers[0] == NULL) {
+        return false;
+    }
+
+    inference.buffers[1] = (signed short *)malloc(n_samples * sizeof(signed short));
+
+    if (inference.buffers[1] == NULL) {
+        ei_free(inference.buffers[0]);
+        return false;
+    }
+
+    // sampleBuffer = (signed short *)malloc((n_samples >> 1) * sizeof(signed short));
+
+    // if (sampleBuffer == NULL) {
+    //     ei_free(inference.buffers[0]);
+    //     ei_free(inference.buffers[1]);
+    //     return false;
+    // }
+
+    inference.buf_select = 0;
+    inference.buf_count = 0;
+    inference.n_samples = n_samples;
+    inference.buf_ready = 0;
+
+    // configure the data receive callback
+    PDM.onReceive(&pdm_data_ready_inference_callback);
+
+    // optionally set the gain, defaults to 24
+    // Note: values >=52 not supported
+    //PDM.setGain(40);
+
+    PDM.setBufferSize(2048);
+
+    // initialize PDM with:
+    // - one channel (mono mode)
+    if (!PDM.begin(1, EI_CLASSIFIER_FREQUENCY)) {
+        ei_printf("ERR: Failed to start PDM!");
+        return false;
+    }
+
+    record_ready = true;
+
+    return true;
+}
+
+/**
+ * @brief      Wait on new data
+ *
+ * @return     True when finished
+ */
+static bool microphone_inference_record(void)
+{
+    bool ret = true;
+
+    if (inference.buf_ready == 1) {
+        ei_printf(
+            "Error sample buffer overrun. Decrease the number of slices per model window "
+            "(EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW)\n");
+        ret = false;
+    }
+
+    while (inference.buf_ready == 0) {
+        delay(1);
+    }
+
+    inference.buf_ready = 0;
+
+    return ret;
+}
+
+/**
+ * Get raw audio signal data
+ */
+static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    numpy::int16_to_float(&inference.buffers[inference.buf_select ^ 1][offset], out_ptr, length);
+
+    return 0;
+}
+
+/**
+ * @brief      Stop PDM and release buffers
+ */
+static void microphone_inference_end(void)
+{
+    PDM.end();
+    ei_free(inference.buffers[0]);
+    ei_free(inference.buffers[1]);
+    // ei_free(sampleBuffer);
+    record_ready = false;
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
